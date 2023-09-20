4aae4276a986efb15875ebd30582bbbdf951b234
QuanL <quan.liu@outlook.fr>
2023-09-20 23:20:54 +0200

    update pep8

--- src/test/test_file.py
+++ src/test/test_file.py
@@ -1,22 +1,23 @@
 # set path
 import sys
 import path 
-directory = path.Path(__file__).abspath()
-sys.path.append(directory.parent.parent)
 import pytest
 import file 
+directory = path.Path(__file__).abspath()
+sys.path.append(directory.parent.parent)
 
 def test_add():
     expected = 5
-    result = file.add(2,3)
+    result = file.add(2, 3)
     assert expected == result
 
 def test_substraction():
     expected = 4
-    result = file.substraction(10,6)
+    result = file.substraction(10, 6)
     assert expected == result
 
 def test_hello():
     expected = 'say Hi'
     result = file.hello('Hi')
     assert expected == result
+    
